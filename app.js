7c621cce777e05ee3b9cfd229eb343530c8614c5
齊藤裕介 <saitoyusuke@saitouyuusukenoMacBook-Pro.local>
2017-09-14 22:58:29 +0900

    SP用のデザインに改修

--- app.js
+++ app.js
@@ -8,15 +8,12 @@ app.set('views', 'views');
 app.set('view engine', 'ejs');
 
 /* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
-var server = app.listen(3000, function(){
+var server = app.listen(3001, function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
 app.use(express.static('public'));
 
-app.get("/lifull/index", function(req, res, next){
+app.get("/calc_game/index", function(req, res, next){
     res.render('index.ejs');
-});
-app.get("/lifull/index2", function(req, res, next){
-    res.render('index2.ejs');
 });
